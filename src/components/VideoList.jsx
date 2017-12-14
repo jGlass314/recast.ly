ba6f0a5c6a0932bc1fe1449b2be7e0708b37763d
jglass314 <jGlass314@gmail.com>
2017-12-13 17:44:41 -0800

    got videoList and videoListEntry to work

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,53 @@
-var VideoList = () => (
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoListEntryArray = [];
+    console.log(props);
+  }
+
+  render() {
+    return (
+      <div className="video-list">
+        {this.props.videoData.map((videoObj) => 
+          <VideoListEntry video={videoObj} />
+        )}
+        <div><VideoListEntry video={this.props.videoData[0]}/></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+        <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div>{props.videoListEntries[0]}</div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -17,3 +64,22 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+//console.log("data not in function", window.exampleVideoData);
+var VideoListApp = function() {
+  var videoListEntryArray = [];
+  //console.log("data in function", window.exampleVideoData);
+  window.exampleVideoData.forEach(function(videoObj) {
+
+    var videoListEntry = new VideoListEntry({
+      videoId: videoObj.id.videoId,
+      title: videoObj.snippet.title,
+      description: videoObj.snippet.description,
+      thumbnail: videoObj.snippet.thumbnails.default.url
+    });
+    videoListEntryArray.push(videoListEntry);
+  });
+  <VideoList videoListEntries={videoListEntryArray}/>;
+};
+window.VideoListApp = VideoListApp;
+*/
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 // TODO: Render the `App` component to the DOM
-
+console.log(document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
